ee23fe8a13e412a0e9169ef37c832914e7491687
Jake Elsberry <jelsberry96@gmail.com>
2023-10-26 11:43:26 -0700

    have full CRUD and model validations

--- CretaceousApi/Models/Animal.cs
+++ CretaceousApi/Models/Animal.cs
@@ -1,16 +1,21 @@
-
-
+using System.ComponentModel.DataAnnotations;
 
 namespace CretaceousApi.Models
 {
   public class Animal
   {
     public int AnimalId { get; set; }
-    
+
+    [Required]
+    [StringLength(30, ErrorMessage = "Name must be between 1 and 30 characters.")]
     public string Name { get; set; }
-    
+
+    [Required]
+    [StringLength(30, ErrorMessage = "Species must be between 1 and 30 characters.")]
     public string Species { get; set; }
-   
+
+    [Required]
+    [Range(0, 200, ErrorMessage = "Age must be between 0 and 200.")]
     public int Age { get; set; }
   }
 }
