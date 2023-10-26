32e7251b0edab5f2843a43c45c2c200dc5fff325
Jake Elsberry <jelsberry96@gmail.com>
2023-10-26 10:06:20 -0700

    can build project and see endpoints in swagger UI

--- CretaceousApi/Models/Animal.cs
+++ CretaceousApi/Models/Animal.cs
@@ -1,10 +1,17 @@
+
+
+
 namespace CretaceousApi.Models
 {
   public class Animal
   {
     public int AnimalId { get; set; }
+    
     public string Name { get; set; }
+    
     public string Species { get; set; }
+   
+   
     public int Age { get; set; }
   }
 }
